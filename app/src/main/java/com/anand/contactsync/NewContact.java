2781a389950e9a2bd9697843e1cf814fd866db6b
Anand <t20anand@gmail.com>
2017-07-28 23:07:04 +0545

    add new contact layout

---
+++ app/src/main/java/com/anand/contactsync/NewContact.java
@@ -0,0 +1,13 @@
+package com.anand.contactsync;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_contact);
+    }
+}
