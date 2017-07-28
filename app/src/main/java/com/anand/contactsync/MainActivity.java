c61c00946976867f4b9572e86022d95b4a8a0682
Anand <t20anand@gmail.com>
2017-07-28 23:27:34 +0545

    layout

--- app/src/main/java/com/anand/contactsync/MainActivity.java
+++ app/src/main/java/com/anand/contactsync/MainActivity.java
@@ -1,5 +1,6 @@
 package com.anand.contactsync;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,12 +19,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton addNewContact = (FloatingActionButton) findViewById(R.id.fab);
+        addNewContact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(view.getContext(), NewContact.class));
             }
         });
     }
